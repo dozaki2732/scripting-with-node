aed39b376ccee0e446b8bf46a4faf6705e539ca1
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-11 20:03:47 -0700

    starting

---
+++ basic-scrape.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const { getComponents, getName, getDesc, trim, getInputs } = require("./regex");
+
+const sourceFile = String(fs.readFileSync("html-pages/basic-functions.html"));
+
+const components = getComponents(sourceFile);
+
+const componentObjs = components.map((component) => {
+   return {
+      name: getName(component)[0],
+      desc: trim(getDesc(component)[0]), //get descript of a component that is an array > get first index > trim
+      inputs: getInputs(component).length,
+      type: "basic", // scraping basic util lib file
+      typeNum: 100, //designated for basic
+      isFavorite: false, // default false
+   };
+});
+
+const reversedObjs = componentObjs.reverse();
+
+const orderedObjs = [];
+for (let i = 0; i < reversedObjs.length; i++) {
+   // add notes from video
+   const obj = reversedObjs[i];
+   obj.order = obj.typeNum + i;
+   orderedObjs.push(obj);
+}
+
+console.log(orderedObjs);
+
+const targetFile = "./JSON/basic.json";
+
+fs.writeFileSync(targetFile, JSON.stringify(orderedObjs));
